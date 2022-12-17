979cd5c2563a435e3a844eb8818f663e3d7aea99
Robin Blondin <robin.blondin@protonmail.com>
2022-12-17 13:17:05 +0100

    Solved day1 puzzle part 1 and 2

--- advent-of-code-2022/day1/day1.js
+++ advent-of-code-2022/day1/day1.js
@@ -1,15 +1,55 @@
 const fs = require("fs");
-const lines = fs.readFileSync("./input.txt", { encoding: "utf-8"})
+const input = fs.readFileSync("./input.txt", { encoding: "utf-8"})
 .replace(/\r/g, '')
-.trim();
+.split('\n');
 
-function part1()
-{
+function part1(){
 
+    var elfCal = 0;
+    var mostCal = 0;
+
+    for (const line of input)
+    {
+        if (line == '')
+        {
+            if (elfCal > mostCal)
+            {
+                mostCal = elfCal;
+                elfCal = 0;
+            }
+            else
+                elfCal = 0;
+        }
+        else
+            elfCal += parseInt(line);
+    }
+    console.log(mostCal);
 }
 
 function part2()
 {
-	
-}
+    var elfCal = 0;
+    var sum = 0;
+    var elfCalArr = [3, 2, 1];
 
+    for (const line of input)
+    {
+        if (line == '')
+        {
+            if (elfCal > Math.min.apply(Math, elfCalArr))
+            {
+
+                elfCalArr[elfCalArr.indexOf(Math.min.apply(Math, elfCalArr))] = elfCal;
+                elfCal = 0;
+            }
+            else
+                elfCal = 0;
+        }
+        else
+            elfCal += parseInt(line);
+    }
+    sum = elfCalArr[0] + elfCalArr[1] + elfCalArr[2];
+    console.log(sum);
+}
+part1();
+part2();
